b382152904630b6c75a6f3e950cbeb955567350c
ごま <gomayumax@users.noreply.github.com>
2017-12-16 17:19:53 +0900

    非同期で読む感じをざっと作った

--- pages/category/_name.vue
+++ pages/category/_name.vue
@@ -1,7 +1,22 @@
 <template>
+  <div>
+    <ul>
+      <li v-for="file in data">{{file.name}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
+  export default {
+    asyncData ({params}, callback) {
+      axios.get(`http://xn--3ur52o1bb7099d.xn--u9jb933vm9i.com/images.json`)
+        .then((res) => {
+          callback(null, {data: res.data.general})
+        })
+    }
+  }
 </script>
 
 <style>
