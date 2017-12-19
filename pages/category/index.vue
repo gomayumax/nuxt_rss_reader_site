2be621d63a92a1b8fb8a1da7e44efd42dcfa7a57
ごま <gomayumax@users.noreply.github.com>
2017-12-19 11:49:02 +0900

    XMLをparseする

--- pages/category/index.vue
+++ pages/category/index.vue
@@ -2,11 +2,19 @@
   <div class="block">
     <h4>category</h4>
     <el-rate v-model="value" :colors="['#2F3D4D', '#64B486', '#4B7C6E']"></el-rate>
+    <p>{{data.root}}</p>
   </div>
 </template>
 
 <script>
   export default {
+    asyncData ({params}, callback) {
+      var parseString = require('xml2js').parseString
+      var xml = '<root>Hello xml2js!</root>'
+      parseString(xml, (message, res) => {
+        callback(null, {data: res})
+      })
+    },
     data () {
       return {
         value: null
